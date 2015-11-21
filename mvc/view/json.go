678372cb685166f7d513b38b192fe3585bc7111e
Zheng Ming <newzhengming@gmail.com>
2015-11-22 04:49:43 +0800

    changed jsonview from struct to map

--- mvc/view/json.go
+++ mvc/view/json.go
@@ -5,29 +5,29 @@ import (
 	"encoding/json"
 	"github.com/zaolab/sunnified/mvc"
 	"github.com/zaolab/sunnified/web"
+	"reflect"
 )
 
-type JsonView struct {
-	mvc.VM
-}
+type JsonView mvc.VM
 
-func (this *JsonView) ContentType(ctxt *web.Context) string {
+func (this JsonView) ContentType(ctxt *web.Context) string {
 	return "application/json; charset=utf-8"
 }
 
-func (this *JsonView) Render(ctxt *web.Context) ([]byte, error) {
-	if this.VM == nil {
-		this.VM = mvc.VM{}
+func (this JsonView) Render(ctxt *web.Context) ([]byte, error) {
+	if this == nil || len(this) == 0 {
+		return []byte{'{', '}'}, nil
 	}
+
 	buf := bytes.NewBuffer(make([]byte, 0, 100))
 	jsone := json.NewEncoder(buf)
-	if err := jsone.Encode(this.VM); err != nil {
+	if err := jsone.Encode(this.getEncodingInterface()); err != nil {
 		return nil, err
 	}
 	return buf.Bytes(), nil
 }
 
-func (this *JsonView) RenderString(ctxt *web.Context) (string, error) {
+func (this JsonView) RenderString(ctxt *web.Context) (string, error) {
 	b, err := this.Render(ctxt)
 	if err == nil {
 		return string(b), nil
@@ -35,50 +35,87 @@ func (this *JsonView) RenderString(ctxt *web.Context) (string, error) {
 	return "", err
 }
 
-func (this *JsonView) Publish(ctxt *web.Context) error {
-	if this.VM == nil {
-		this.VM = mvc.VM{}
-	}
+func (this JsonView) Publish(ctxt *web.Context) error {
 	ctxt.SetHeader("Content-Type", "application/json; charset=utf-8")
+
+	if this == nil || len(this) == 0 {
+		ctxt.Response.Write([]byte{'{', '}'})
+		return nil
+	}
+
 	jsone := json.NewEncoder(ctxt.Response)
-	if err := jsone.Encode(this.VM); err != nil {
+	if err := jsone.Encode(this.getEncodingInterface()); err != nil {
 		return err
 	}
 	return nil
 }
 
-type FullJsonView struct {
-	*JsonView
+func (this JsonView) getEncodingInterface() (i interface{}) {
+	i = this
+
+	if len(this) == 1 && this[""] != nil {
+		t := reflect.TypeOf(this[""])
+
+		if kind := t.Kind(); kind == reflect.Map || kind == reflect.Struct ||
+			(kind == reflect.Ptr && t.Elem().Kind() == reflect.Struct) {
+			i = this[""]
+		}
+	}
+
+	return
+}
+
+type FullJsonView JsonView
+
+func (this FullJsonView) ContentType(ctxt *web.Context) string {
+	return JsonView(this).ContentType(ctxt)
+}
+
+func (this FullJsonView) Render(ctxt *web.Context) ([]byte, error) {
+	return JsonView(this).Render(ctxt)
+}
+
+func (this FullJsonView) RenderString(ctxt *web.Context) (string, error) {
+	return JsonView(this).RenderString(ctxt)
+}
+
+func (this FullJsonView) Publish(ctxt *web.Context) error {
+	return JsonView(this).Publish(ctxt)
 }
 
 func (this *FullJsonView) SetVMap(vmap ...mvc.VM) {
-	if this.JsonView == nil {
-		this.JsonView = NewJsonView(nil)
+	if *this == nil {
+		*this = NewFullJsonView(nil)
 	}
+
+	_vmap := *this
+
 	for _, vm := range vmap {
 		for k, v := range vm {
-			this.VM[k] = v
+			_vmap[k] = v
 		}
 	}
 }
 
 func (this *FullJsonView) SetData(name string, value interface{}) {
-	if this.JsonView == nil {
-		this.JsonView = NewJsonView(nil)
+	if *this == nil {
+		*this = NewFullJsonView(nil)
 	}
-	this.VM[name] = value
+
+	_vmap := *this
+	_vmap[name] = value
 }
 
-func NewJsonView(vmap mvc.VM) *JsonView {
+func NewJsonView(vmap mvc.VM) JsonView {
 	if vmap == nil {
 		vmap = mvc.VM{}
 	}
-	return &JsonView{vmap}
+	return JsonView(vmap)
 }
 
-func NewFullJsonView(vmap mvc.VM) *FullJsonView {
+func NewFullJsonView(vmap mvc.VM) FullJsonView {
 	if vmap == nil {
 		vmap = mvc.VM{}
 	}
-	return &FullJsonView{NewJsonView(vmap)}
+	return FullJsonView(NewJsonView(vmap))
 }
